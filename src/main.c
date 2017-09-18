1c70c5ff355e2b193e23ae2f9d8b5ed209a92ea4
Martin Kobelka <martinkobelka@gmail.com>
2017-09-18 22:29:35 +0200

    Add simple skeleton of project

---
+++ src/main.c
@@ -0,0 +1,22 @@
+
+// Include header files
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main function, it called when program starts
+ *
+ * @param int argc count of arguments
+ * @param char** argv Array of arguments
+ *
+ * @return int 0 when everything is OK or other number
+ */
+int main(int argc, char **argv) {
+
+    // Print text to the console
+    printf("Hello world\n");
+
+    // Return 0
+    return EXIT_SUCCESS;
+
+}
